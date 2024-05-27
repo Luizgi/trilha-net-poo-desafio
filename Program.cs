d8b2f06d65d673c9b977018b084cf20f2a9da2f2
Luiz Guilherme <irringra@gmail.com>
2024-05-27 10:19:30 -0300

    projeto finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Teste no Nokia: ");
+Smartphone nokia = new Nokia("777", "Tijolo", "237", 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Pou");
+
+Console.WriteLine("Teste no Iphone: ");
+Smartphone iphone = new Iphone("1515", "Iphone 13", "887", 512);
+iphone.Ligar();
+iphone.InstalarAplicativo("Amazon Store");
